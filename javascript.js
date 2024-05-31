277d2f8404ab8238a29713a0c36cf3f4b9238aaf
Tauri <145342716+8Tauri8@users.noreply.github.com>
2024-05-31 11:36:44 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
+
 function advice() {
 let fetchRes = fetch("https://api.adviceslip.com/advice");
     fetchRes.then(res => res.json())
-    .then(d => {console.log(d)})
+    .then(d => {document.getElementById('adviceParagraph').textContent = d.slip.advice;})
 }
 
