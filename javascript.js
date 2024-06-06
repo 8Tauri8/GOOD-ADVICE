f4a50fb9ed6c32896e5b9bca9004ae8d73be674a
Tauri <145342716+8Tauri8@users.noreply.github.com>
2024-06-06 23:37:41 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
+const button = document.getElementById('btn');
 
-function advice() {
-let fetchRes = fetch("https://api.adviceslip.com/advice");
-    fetchRes.then(res => res.json())
-    .then(d => {document.getElementById('adviceParagraph').textContent = d.slip.advice;})
-}
+button.addEventListener('mouseover', function ()
+ {
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+    console.log("hehe")
+});
+
+
+button.addEventListener("click", function ()
+{
+  let fetchRes = fetch("https://api.adviceslip.com/advice");
+   fetchRes.then(res => res.json())
+ .then(d => {document.getElementById('adviceParagraph').textContent = d.slip.advice;})
+});
+
 
